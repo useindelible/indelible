3d13b3d1e5e52ea69eaaacc6f984b10036d59692
Samuel Ajisegiri <samajisegiri@gmail.com>
2026-08-23 10:57:44 +0100

    fix(web): keep author follow action compact

--- web/src/lib/components/library/AuthorCard.svelte
+++ web/src/lib/components/library/AuthorCard.svelte
@@ -50,7 +50,7 @@
 				d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"
 			/>
 		</svg>
-		{$t('library_author_follow_unavailable')}
+		{$t('library_author_follow')}
 	</button>
 </div>
 
